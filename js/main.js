ab870a3a766497eb32c011d1c981ebc9a63cb15e
Ali ElHero <124408599+AliElhero@users.noreply.github.com>
2023-02-07 13:37:05 +0200

    Fixed

--- js/main.js
+++ js/main.js
@@ -1,31 +1,15 @@
-let title=document.querySelector("#title"),price=document.querySelector("#price"),ads=document.querySelector("#ads"),tax=document.querySelector("#tax"),discount=document.querySelector("#discount"),cont=document.querySelector("#cont"),category=document.querySelector("#category"),create_btn=document.querySelector("#create"),search_bar=document.querySelector("#search_bar"),search_title_btn=document.querySelector("#search_title"),search_category_btn=document.querySelector("#search_category"),delete_all_btn=document.querySelector("#delete_all"),total_price=document.querySelector("#total_price"),getTotal=()=>{if(price.value.length>0&&ads.value.length>0&&tax.value.length>0&&discount.value.length>0){let t=+price.value+ +ads.value+ +tax.value-+discount.value;total_price.innerHTML=t,total_price.style.color="green"}else total_price.style.color="red",total_price.innerHTML="00.00"},dataPro;function deleteData(t){dataPro.splice(t,1),localStorage.products=JSON.stringify(dataPro),dataPro.length<=0&&(document.querySelector("#refresh").style.display="block"),showData()}function clearData(){title.value="",price.value="",ads.value="",tax.value="",discount.value="",total_price.innerHTML="",cont.value="",category.value=""}function showData(){let t="";for(let e=0;e<dataPro.length;e++)t+=`
-        <tr>
-                                    <td><span>#</span>${e}</td>
-                                    <th>${dataPro[e].title}</th>
-                                    <th>${dataPro[e].price}<span>$</span></th>
-                                    <th>${dataPro[e].tax}<span>$</span></th>
-                                    <th>${dataPro[e].ads}<span>$</span></th>
-                                    <th>${dataPro[e].discount}<span>$</span></th>
-                                    <th>${dataPro[e].total}<span>$</span></th>
-                                    <th>${dataPro[e].category}</th>
-                                    <th>
-                                        <button
-                                            style="background-color: #1134a6"
-                                            class="btn"
-                                            id="update"
-                                        >
-                                            Update
-                                        </button>
-                                    </th>
-                                    <th>
-                                        <button
-                                            style="background-color: #1134a6"
-                                            class="btn"
-                                            id="delete"
-                                            onclick="deleteData(    ${e}    )"
-                                        >
-                                            Delete
-                                        </button>
-                                    </th>
-                                </tr>
-        `,document.querySelector("#tbody").innerHTML=t;dataPro.length>0?(delete_all_btn.style.display="block",document.querySelector("#num_pro").innerHTML=dataPro.length):delete_all_btn.style.display="none"}dataPro=null!=localStorage.products?JSON.parse(localStorage.products):[],showData(),create_btn.addEventListener("click",()=>{let t={title:title.value,price:price.value,ads:ads.value,tax:tax.value,discount:discount.value,total:total_price.innerHTML,cont:cont.value,category:category.value};if(t.cont>1)for(let e=0;e<t.cont;e++)dataPro.push(t);else dataPro.push(t);localStorage.setItem("products",JSON.stringify(dataPro)),clearData(),showData()}),delete_all_btn.addEventListener("click",()=>{localStorage.clear(),dataPro.splice(0),showData(),document.querySelector("#refresh").style.display="block"}),showData();
+let title=document.querySelector("#title");let price=document.querySelector("#price");let ads=document.querySelector("#ads");let tax=document.querySelector("#tax");let discount=document.querySelector("#discount");let cont=document.querySelector("#cont");let category=document.querySelector("#category");let create_btn=document.querySelector("#create");let search_bar=document.querySelector("#search_bar");let search_title_btn=document.querySelector("#search_title");let search_category_btn=document.querySelector("#search_category");let delete_all_btn=document.querySelector("#delete_all");let total_price=document.querySelector("#total_price");let getTotal=()=>{if(price.value.length>0&&ads.value.length>0&&tax.value.length>0&&discount.value.length>0){let result=+price.value++ads.value++tax.value-+discount.value;total_price.innerHTML=result;total_price.style.color="green";}else{total_price.style.color="red";total_price.innerHTML="00.00";}};let dataPro;if(localStorage.products!=null){dataPro=JSON.parse(localStorage.products);}else{dataPro=[];}
+showData();create_btn.addEventListener("click",()=>{let newPro={title:title.value,price:price.value,ads:ads.value,tax:tax.value,discount:discount.value,total:total_price.innerHTML,cont:cont.value,category:category.value,};if(newPro.cont>1){for(let i=0;i<newPro.cont;i++){dataPro.push(newPro);}}else{dataPro.push(newPro);}
+localStorage.setItem("products",JSON.stringify(dataPro));clearData();showData();});function deleteData(i){dataPro.splice(i,1);localStorage.products=JSON.stringify(dataPro);if(dataPro.length<=0){document.querySelector("#refresh").style.display="block";}
+showData();}
+delete_all_btn.addEventListener("click",()=>{localStorage.clear();dataPro.splice(0);showData();document.querySelector("#refresh").style.display="block";});function clearData(){title.value="";price.value="";ads.value="";tax.value="";discount.value="";total_price.innerHTML="";cont.value="";category.value="";}
+function showData(){let table="";for(let i=0;i<dataPro.length;i++){table+=`<tr><td><span>#</span>${i}</td><th>${dataPro[i].title}</th><th>${dataPro[i].price}<span>$</span></th><th>${dataPro[i].tax}<span>$</span></th><th>${dataPro[i].ads}<span>$</span></th><th>${dataPro[i].discount}<span>$</span></th><th>${dataPro[i].total}<span>$</span></th><th>${dataPro[i].category}</th><th><button
+style="background-color: #1134a6"
+class="btn"
+id="update">Update</button></th><th><button
+style="background-color: #1134a6"
+class="btn"
+id="delete"
+onclick="deleteData(    ${i}    )">Delete</button></th></tr>`;document.querySelector("#tbody").innerHTML=table;}
+if(dataPro.length>0){delete_all_btn.style.display="block";document.querySelector("#num_pro").innerHTML=dataPro.length;}else{delete_all_btn.style.display="none";}}
+showData();
